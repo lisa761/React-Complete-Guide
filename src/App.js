e675b4cc7762c6dc531c790483fbb06918b2f6ff
Lisa Chandra <liza761c@gmail.com>
2021-02-19 00:19:08 +0530

    add: props, state, setState

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person'
 
 class App extends Component {
+  state = {
+    persons : [
+      { name: 'Max', age: 28 },
+      { name: 'Manu', age: 29 },
+      { name: 'Stephanie', age: 26 }
+    ],
+    otherState: 'some other value'
+  }
+
+  switchNameHandler = () => {
+    // DON'T DO THIS: this.state.persons[0].name = 'Maximilian';
+    this.setState({
+      persons : [
+        { name: 'Maximilian', age: 28 },
+        { name: 'Manu', age: 29 },
+        { name: 'Stephanie', age: 26 }
+      ]
+    })
+    // takes the object and merges it with the current state
+  }
+
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm React App!!!</h1>
+        <p>This is really working!</p>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Reading</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
       </div>
-      // above jsx transpiles to:
-      // return React.creatElement('div', {className='App', React.createElement('h1', null, 'Hi, I'm a React App!!!)})
     );
   }
 }
