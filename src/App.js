1f20583dbb025559dcf2255f0dc9fcff8b55c579
Lisa Chandra <liza761c@gmail.com>
2021-02-19 03:22:14 +0530

    add: made nameChangedHandler dynamic and added key to the list

--- src/App.js
+++ src/App.js
@@ -5,31 +5,34 @@ import Person from './Person/Person';
 class App extends Component {
   state = {
     persons: [
-      { name: 'Max', age: 28 },
-      { name: 'Manu', age: 29 },
-      { name: 'Stephanie', age: 26 }
+      { id: 'asdf', name: 'Max', age: 28 },
+      { id: 'dsfh', name: 'Manu', age: 29 },
+      { id: 'wert', name: 'Stephanie', age: 26 }
     ],
     otherState: 'some other value',
     showPersons: false
   }
 
-  nameChangedHandler = (event) => {
-    this.setState({
-      persons : [
-        { name: 'Max', age: 28 },
-        { name: event.target.value, age: 29 },
-        { name: 'Stephanie', age: 26 }
-      ]
-    })
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return id === p.id;
+    });
+
+    // const person = Object.assign({}, this.state.persons[personIndex]); // old way
+    const person = {
+      ...this.state.persons[personIndex]
+    }
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({persons : persons});
   }
 
   deletePersonHandler = (personIndex) => {
-    // const persons = this.state.persons;
-    // bad practice because the splice line is then adding changes to the original value and updating it would mean mutating the original value which can lead to unpredictible behaviour. using splice instead would make sure that this persons is a copy and the not the original value. making changes to it and then setting new state would set the value of persons to a completely new array.
-    // const persons = this.state.persons.slice();
-    const persons = [...this.state.persons]; // ES 6 version of above
+    const persons = [...this.state.persons];
     persons.splice(personIndex, 1);
-    // here we aren't changing the value of persons which is a const: since persons is an array and arrays are reference types that merely store a pointer to the location where the value is stored, we are technically only changing the value and not the stored pointer.
     this.setState({persons: persons});
   }
 
@@ -55,10 +58,13 @@ class App extends Component {
             return <Person
             click={() => this.deletePersonHandler(index)}
             name={person.name}
-            age={person.age} />
+            age={person.age}
+            key={person.id}
+            changed={(event) => this.nameChangedHandler(event, person.id)} />
           })}
         </div>
       );
+      // keys are important in lists so react can go through the lists deeply to update the dom instead of re-rendering the entire list
     }
 
     return (
